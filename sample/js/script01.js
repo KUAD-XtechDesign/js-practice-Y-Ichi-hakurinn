69bdeed64314b725e65b7512bbbc2cb68feb24d5
Y-Ichi-hakurinn <y-ichikawa0270@st.kyoto-art.ac.jp>
2021-10-08 15:58:17 +0900

    Update script01.js

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,18 +1,18 @@
 $(function(){
 
     $("#button01").on("click",function(){
-        $("back-red").hide();
+        $("back - red").hide();
     })
 
     $("#button02").on("click",function(){
-        $("back-blue").hide();
+        $("back - blue").hide();
     })
 
     $("#button03").on("click",function(){
         $("#box03,#box07,#box08").hide();
     })
 
-    $("#button01").on("click",function(){
+    $("#button04").on("click",function(){
         $(".box").show();
     })
 
